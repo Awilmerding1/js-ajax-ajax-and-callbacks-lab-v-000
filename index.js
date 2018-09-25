a5b399c686a8957b4a312b4919fa389243d9880e
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-09-25 16:45:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ $(document).ready(function (){
   function searchRepositories() {
     let terms = $('#searchTerms').val()
     $.get(`https://api.github.com/search/repositories?q=${terms}`, function(repoData) {
-      console.log(repoData)
       $('#results').html(
         repoData.items.map(function(repo) {
           return `<div style="display:flex"><img src="${repo.owner.avatar_url}", style="max-width:100px;max-height:100px;float:left;"><p style="padding-left:15px"><a href="${repo.html_url}">${repo.name}</a> - ${repo.description} - <a href="${repo.owner.html_url}">${repo.owner.login}</a> - <a href="#" data-repository="${repo.name}" data-owner="${repo.owner.login}" onclick="showCommits(this)">Show Commits</a></p></div>` 
